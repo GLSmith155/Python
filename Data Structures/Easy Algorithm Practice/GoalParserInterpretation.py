c6676b63932b409b1a4d100a45abc66d471976f1
George Smith <40671362+GLSmith155@users.noreply.github.com>
2021-11-16 14:07:41 -0500

    Create GoalParserInterpretation.py

---
+++ Data Structures/Easy Algorithm Practice/GoalParserInterpretation.py
@@ -0,0 +1,5 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        return command.replace("()", "o").replace("(al)", "al")
+      
+          # .replace("x", "y") is Python's String replace method, and it replaces x with y in a given string. In this case, a string already exists with the name 'command'.
