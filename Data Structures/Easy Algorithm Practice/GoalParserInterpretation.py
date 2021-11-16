5c1f229d8f714fddba147aee4d5128c8667dde01
George Smith <40671362+GLSmith155@users.noreply.github.com>
2021-11-16 14:13:32 -0500

    Update GoalParserInterpretation.py

--- Data Structures/Easy Algorithm Practice/GoalParserInterpretation.py
+++ Data Structures/Easy Algorithm Practice/GoalParserInterpretation.py
@@ -3,3 +3,4 @@ class Solution:
         return command.replace("()", "o").replace("(al)", "al")
       
           # .replace("x", "y") is Python's String replace method, and it replaces x with y in a given string. In this case, a string already exists with the name 'command'.
+          # count is optional for .replace. A number specifying how many occurrences of the old value you want to replace. Default is all occurrences
