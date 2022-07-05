7d8a503b44f025d60c7aec65ceaee0e2d28bc660
George Smith <40671362+GLSmith155@users.noreply.github.com>
2022-07-05 10:48:11 -0500

    Update Roman to Integer.py

--- Leetcode/Roman to Integer.py
+++ Leetcode/Roman to Integer.py
@@ -1,3 +1,24 @@
+#Time: O(n)
+#Space: O(128) = O(1)
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+
+        for a, b in zip(s, s[1:]):
+            if roman[a] < roman[b]:
+                ans -= roman[a]
+            else:
+                ans += roman[a]
+
+        return ans + roman[s[-1]]
+
+
+
+
+
+
 # Time complexity of O(1).
 
 class Solution:
